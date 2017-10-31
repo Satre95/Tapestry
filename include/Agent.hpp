81c2b8a12842fc552d3cabd97e08e8dde3bbfcb6
Siddharth Atre <sidatre@gmail.com>
2017-10-30 18:41:53 -0700

    Setup for agent advancement.

--- include/Agent.hpp
+++ include/Agent.hpp
@@ -1,12 +1,21 @@
 #pragma once
-#include <cinder/GeomIo.h>
+
+#include <functional>
+#include "cinder/GeomIo.h"
+#include "Trail.hpp"
+#include "SpatialHashTable.hpp"
 
 class Agent
 {
 public:
-	Agent(ci::vec3 pos);
+	Agent(ci::vec3 pos, std::function<ci::vec3(ci::vec3&)>  advanceFn = defaultAdvanceFn);
 	~Agent();
 
+	ci::vec3 Advance(const SpatialHashTable<std::pair<size_t, size_t>> & trails);
+	void Draw();
 private:
 	ci::vec3 m_position;
+	Trail m_trail;
+	/// The default advancement algo, which randomly samples one of the six cardinal directions.
+	static std::function< ci::vec3(ci::vec3)> defaultAdvanceFn;
 };
--- src/Agent.cpp
+++ src/Agent.cpp
@@ -1,9 +1,23 @@
 #include "Agent.hpp"
-
+#include "cinder/Rand.h"
 using namespace ci;
 
-Agent::Agent(vec3 pos) : m_position(pos) {
+std::function< ci::vec3(ci::vec3)> Agent::defaultAdvanceFn = [](ci::vec3 oldPos) {
+	return oldPos;
+};
+
+Agent::Agent(ci::vec3 pos, std::function<ci::vec3(ci::vec3&)>  advanceFn) {
+	m_position = pos;
 }
 
 Agent::~Agent() {
+}
+
+vec3 Agent::Advance(const SpatialHashTable<std::pair<size_t, size_t>> & trails) {
+	m_trail.AddPoint(m_position);
+
+	return m_position;
+}
+
+void Agent::Draw() {
 }
